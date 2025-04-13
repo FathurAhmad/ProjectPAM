a310cd60621ef1eca88741a0a9dbdfd6f2a054e8
DewaGedeR <dewagederafel@gmail.com>
2025-04-13 22:26:21 +0700

    malas cik

--- app/src/main/java/com/example/projectpambaru/dashboard.java
+++ app/src/main/java/com/example/projectpambaru/dashboard.java
@@ -11,14 +11,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class dashboard extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TransaksiAdapter adapter;
+    List<TransaksiItem> transaksiList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.dashboard_layout);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        transaksiList = new ArrayList<>();
+        transaksiList.add(new TransaksiItem("Pengeluaran", "Transaksi ke-1", "Membeli kopi", "Rp 20.000"));
+        transaksiList.add(new TransaksiItem("Pengeluaran", "Transaksi ke-2", "Bayar parkir", "Rp 5.000"));
+        transaksiList.add(new TransaksiItem("Pengeluaran", "Transaksi ke-3", "Makan siang", "Rp 30.000"));
+
+        adapter = new TransaksiAdapter(transaksiList);
+        recyclerView.setAdapter(adapter);
 
         Button home = findViewById(R.id.home_button);
         Button income = findViewById(R.id.uang_masuk_button);

2580dc16037c4a345156e4002f1cd74775dd4471
dew <dewagederafel@gmail.com>
2025-04-20 20:58:30 +0700

    Nambahi profile

--- app/src/main/java/com/example/projectpambaru/dashboard.java
+++ app/src/main/java/com/example/projectpambaru/dashboard.java
@@ -1,13 +1,19 @@
 package com.example.projectpambaru;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.LayoutInflaterCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -47,6 +53,27 @@ public class dashboard extends AppCompatActivity {
         Button income = findViewById(R.id.uang_masuk_button);
         Button outcome = findViewById(R.id.uang_keluar_button);
         Button target = findViewById(R.id.target_button);
+        ImageView iconProfile = findViewById(R.id.profile_image);
+
+        iconProfile.setOnClickListener(v -> {
+            LayoutInflater inflater = getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.dialog_profile, null);
+
+            ImageView imageViewProfile = dialogView.findViewById(R.id.imageViewProfile);
+            TextView textViewUbahProfile = dialogView.findViewById(R.id.textViewUbahFoto);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(dialogView);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            textViewUbahProfile.setOnClickListener(v2 -> {
+                Intent profileIntent = new Intent(Intent.ACTION_PICK);
+                profileIntent.setType("image/*");
+                startActivityForResult(profileIntent, 100);
+                dialog.dismiss();
+            });
+        });
 
         home.setOnClickListener(v -> {
             Intent intent = new Intent(dashboard.this, dashboard.class);
@@ -77,5 +104,16 @@ public class dashboard extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            // Ganti imageView dengan ID dari profil utama kalau kamu punya
+            ImageView imageViewUtama = findViewById(R.id.profile_image);
+            imageViewUtama.setImageURI(selectedImage);
+        }
     }
 }
